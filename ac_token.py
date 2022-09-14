89790b1f0bf5d02ffc7ebc6afd570117f9cf91e8
倒霉的阿尖 <65476520+willni94@users.noreply.github.com>
2022-09-15 01:41:11 +0800

    Update ac_token.py

--- ac_token.py
+++ ac_token.py
@@ -2,7 +2,7 @@
 import requests
 
 # client_id 为官网获取的AK， client_secret 为官网获取的SK
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=14YSKWAtBiNb6MasLUR6PR0e&client_secret=Gddb0KsYXuO3PayQdStnBApy5jAtcn8q'
+host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=填入你的APIKEYclient_secret=填入你的SecretKEY'
 response = requests.get(host)
 if response:
     print(response.json())
